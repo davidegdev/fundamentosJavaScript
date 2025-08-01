a02d656c02fdeb689c97bbfd5fdccdc6c337bfdb
David Escorcia Gomez <descorci@bancolombia.com.co>
2025-08-01 15:51:28 -0500

    Add examples and console logs for integer, decimal, scientific notation, infinity, and NaN

---
+++ clase-numbers.js
@@ -0,0 +1,15 @@
+// 1. Tipo enetero y decimal
+let entero = 43
+let decimal = 43.10
+console.log(typeof entero, typeof decimal)
+
+// 2. Notacion científica
+const cientifico = 5e3
+console.log(cientifico, typeof cientifico)
+
+// 3. Infinitos y NaN
+let infinito = Infinity
+let noEsUnNumero = NaN
+console.log(infinito, typeof infinito)
+console.log(noEsUnNumero, typeof noEsUnNumero)
+
