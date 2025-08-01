c398b5ba222e2298c94cd019d213fc58f1a4aeee
David Escorcia Gomez <descorci@bancolombia.com.co>
2025-08-01 15:32:36 -0500

    Add string data type examples and console logs

---
+++ strings.js
@@ -0,0 +1,16 @@
+// tipo de dato: string
+
+let string1 = 'Hola, Mundo'
+let string2 = "JavaScript es genial"
+let string3 = `${string1} feliz :)`
+console.log(string3)
+
+// tamaño de string
+console.log(string3.length)
+
+// minusculas y mayusculas
+console.log(string3.toLowerCase())
+console.log(string3.toUpperCase())
+
+// imprimir sub-strings
+console.log(string2.substring(0, 10))
