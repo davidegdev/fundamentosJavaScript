f36a4c837ea3e2cccb09537f4412812dc773036a
David Escorcia Gomez <descorci@bancolombia.com.co>
2025-08-01 15:16:16 -0500

    Add initial variable declarations and comments for primitive and complex data types

---
+++ tipos-de-datos.js
@@ -0,0 +1,29 @@
+// PRIMITIVOS
+
+// string
+let nombre = 'David'
+// number
+let edad = 27
+// boolean
+let esMayotDeEdad = true
+// null
+let noHayValor = null
+// undefined
+let noDefinido = undefined
+// symbol
+let simboloUnico = Symbol('único')
+// bigint
+let numeroGrande = 2n
+
+
+// COMPLEJOS
+
+// object
+let carro = {
+    marca: 'Tesla',
+    modelo: 'Model S'
+}
+// array o listas
+let frutas = ['manzana', 'banano', 'uvas']
+// function
+function saludar(){}
