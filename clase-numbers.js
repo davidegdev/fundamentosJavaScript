895b66160754df100dbdc8669eb7e33d07c50e10
David Escorcia Gomez <descorci@bancolombia.com.co>
2025-08-01 16:12:41 -0500

    Fix comment typo and add advanced operations examples in clase-numbers.js

--- clase-numbers.js
+++ clase-numbers.js
@@ -16,8 +16,13 @@ console.log(noEsUnNumero, typeof noEsUnNumero)
 // Solucionando errores de precisión
 let resultado = 0.1 + 0.2
 console.log(resultado)
-console.log(resultado.toFixed(1)) // Solucion,, que nos devuelva un solo decimal.
+console.log(resultado.toFixed(1)) // Solucion, que nos devuelva un solo decimal.
 
 // probando
 console.log((0.1 + 0.2) === 0.3) // false
 console.log(resultado.toFixed(1) === 0.3.toFixed(1)) // Solucion: true
+
+// Operaciones Avanzadas
+console.log(`Raiz cuadrada: ${Math.sqrt(16)}`)
+console.log(`Valor absoluto: ${Math.abs(-7)}`)
+console.log(`Valor random: ${Math.random()}`)
